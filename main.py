5d6db4991307ed62136c32e23e972472b3161c7b
Jan Poonthong <janpoonthong628@gmail.com>
2022-08-28 12:02:18 +0700

    Create folder in zoho

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 from pathlib import Path
 import requests
 import discord
@@ -26,20 +27,11 @@ client = discord.Client(intents=discord.Intents.default())
 async def on_ready():
     print("We have logged in as {0.user}".format(client))
 
-async def get_history_of_channel(channel):
-    messages = await channel.history(limit=123).flatten() # adding None lets us retrieve every message
-    for message in messages:
-        # do something with that message
-        print(message.content)
 
 @client.event
 async def on_message(message: discord.Message):
     channel = client.get_channel(911489493478047758)
-
-    messages = [message async for message in channel.history(limit=123)]
-    print(messages)
-    
-    #await get_history_of_channel(discord.TextChannel)
+    messages = [message async for message in channel.history(limit=None)]
 
     if valid_image_url(message.content):
         await download_image(message.content, "images")
@@ -47,34 +39,82 @@ async def on_message(message: discord.Message):
     for message in messages:
         for attachment in message.attachments:
             current_directory = os.getcwd()
-            final_directory = os.path.join(current_directory + '/images', f'{message.author}')
+            final_directory = os.path.join(
+                current_directory + "/images", f"{message.author}"
+            )
             if not os.path.exists(final_directory):
                 os.makedirs(final_directory)
             print(message.author)
             if valid_image_url(attachment.url):
-                await attachment.save(os.path.join("images" + f'/{message.author}', attachment.filename))
+                await attachment.save(
+                    os.path.join(
+                        "images" + f"/{message.author}", attachment.filename
+                    )
+                )
 
 
 def valid_image_url(url: str):
-    image_extensions = ["png", "jpg", "jpeg", "gif", "PNG", "JPG", "JPEG", "GIF"]
+    image_extensions = [
+        "png",
+        "jpg",
+        "jpeg",
+        "gif",
+        "PNG",
+        "JPG",
+        "JPEG",
+        "GIF",
+    ]
     for image_extension in image_extensions:
         if url.endswith("." + image_extension):
             return True
     return False
 
+
 def zoho_token():
-    url = "https://accounts.zoho.com/oauth/v2/token?refresh_token={os.environ.get('zoho_refresh_token')}&client_secret={os.environ.get('zoho_client_secret')}&grant_type=refresh_token&client_id={os.environ.get('zoho_client_id')}"
+    url = f"https://accounts.zoho.com/oauth/v2/token?refresh_token={os.getenv('zoho_refresh_token')}&client_secret={os.getenv('zoho_client_secret')}&grant_type=refresh_token&client_id={os.getenv('zoho_client_id')}"
     access_token = requests.post(url)
     return access_token["access_token"]
 
 
+def create_folder_zoho():
+    path = "images/"
+    dir_list = os.listdir(path)
+
+    url = "https://www.zohoapis.com/workdrive/api/v1/files"
+    headers = {
+        "Authorization": f"Zoho-oauthtoken {os.getenv('zoho_access_token')}",
+        "Content-Type": "application/json",
+    }
+
+    for dir in dir_list:
+        payload = json.dumps(
+            {
+                "data": {
+                    "attributes": {
+                        "name": f"{dir}",
+                        "parent_id": "p1u2g5369e6ac75d0445e9a8ab10172fc8cee",
+                    },
+                    "type": "files",
+                }
+            }
+        )
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+    return response.text
+
+
+create_folder_zoho()
+
+
 def save_zoho_drive():
     url = "https://www.zohoapis.com/workdrive/api/v1/upload?parent_id=hltaja4afd79bedb04e93bcede5e7e897802f&override-name-exist=true"
+    headers_for_zoho = {
+        "Authorization": f"Zoho-oauthtoken {os.getenv('zoho_access_token')}"
+    }
+
     for path in Path("./").rglob("*.png"):
         files = {"content": open(f"{path}", "rb")}
-        headers_for_zoho = {
-            "Authorization": f"Zoho-oauthtoken {os.environ.get('file_zoho_token')}"
-        }
         response = requests.post(url, files=files, headers=headers_for_zoho)
         print(response.json())
 
