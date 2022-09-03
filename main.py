867ac0ca2e946de3b8ab528c92ee423dab51a007
Jan Poonthong <janpoonthong628@gmail.com>
2022-09-03 11:34:33 +0700

    Rename variables

--- main.py
+++ main.py
@@ -106,13 +106,15 @@ def list_folders_zoho():
     }
 
     response = requests.request("GET", url, headers=headers)
+    response_handler_500(response)
+
     folder_lists = {}
     response_data = response.json()["data"]
-    print(response_data)
-    if response_data == []:
+
+    if not response_data:
         return folder_lists
 
-    for i in range(len(dir_list)):
+    for i in range(len(response_data)):
         folder_lists[response_data[i]["attributes"]["name"]] = response_data[i][
             "id"
         ]
@@ -121,10 +123,7 @@ def list_folders_zoho():
 
 
 def create_folder_zoho(folder_lists):
-    if not os.path.exists(path):
-        os.mkdir(path)
-
-    if dir_list == []:
+    if not dir_list:
         return
 
     url = "https://www.zohoapis.com/workdrive/api/v1/files"
@@ -134,13 +133,13 @@ def create_folder_zoho(folder_lists):
     }
 
     response = False
-    for dir in dir_list:
+    for local_folder in dir_list:
         if folder_lists == {}:
             payload = json.dumps(
                 {
                     "data": {
                         "attributes": {
-                            "name": f"{dir}",
+                            "name": f"{local_folder}",
                             "parent_id": "p1u2g5369e6ac75d0445e9a8ab10172fc8cee",
                         },
                         "type": "files",
@@ -151,14 +150,14 @@ def create_folder_zoho(folder_lists):
             response = requests.request(
                 "POST", url, headers=headers, data=payload
             )
-        elif folder_lists[dir]:
+        elif folder_lists[local_folder]:
             continue
         else:
             payload = json.dumps(
                 {
                     "data": {
                         "attributes": {
-                            "name": f"{dir}",
+                            "name": f"{local_folder}",
                             "parent_id": "p1u2g5369e6ac75d0445e9a8ab10172fc8cee",
                         },
                         "type": "files",
@@ -182,13 +181,25 @@ def save_zoho_drive(parent_id, folder_name):
         "Authorization": f"Zoho-oauthtoken {os.getenv('zoho_access_token')}"
     }
 
-    images_ext = ["png", "jpeg", "jpg", "gif"]
+    images_ext = [
+        "png",
+        "jpeg",
+        "jpg",
+        "gif",
+        "PNG",
+        "JPG",
+        "JPEG",
+        "GIF",
+    ]
 
     for ext in images_ext:
-        for path in Path(f"images/{folder_name}").rglob(f"*.{ext}"):
-            files = {"content": open(f"{path}", "rb")}
+        for local_path in Path(f"images/{folder_name}").rglob(f"*.{ext}"):
+            files = {"content": open(f"{local_path}", "rb")}
             response = requests.post(url, files=files, headers=headers_for_zoho)
-            print(response.json())
+            print(
+                response_handler_500(response),
+                f"Saved {local_path} in {folder_name} in Zoho",
+            )
 
 
 async def download_image(url: str, images_path: str = ""):
@@ -201,11 +212,11 @@ async def download_image(url: str, images_path: str = ""):
 
 
 def main():
-    create_folder_zoho(list_folders_zoho())
-    temp = list_folders_zoho()
-    for folder_name in temp:
-        save_zoho_drive(temp[folder_name], folder_name)
-    # client.run(os.getenv("TOKEN"))
+    # folder_list = list_folders_zoho()
+    # create_folder_zoho(folder_list)
+    # for folder_name in folder_list:
+        # save_zoho_drive(folder_list[folder_name], folder_name)
+    client.run(os.getenv("TOKEN"))
 
 
 main()
